fc1d52775df7d8783967e43a76298002152f920d
xShadow <103582788+zShadowSkilled1@users.noreply.github.com>
2022-12-18 09:35:07 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 11705040445 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/SoapClient/main/DBBV6.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/SoapClient/main/DBBV6.lua", true))()
 end
