75948ef04741cd0156135cac4dcd81276dae89db
xShadow <103582788+zShadowSkilled1@users.noreply.github.com>
2022-12-19 15:28:22 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,7 @@
 if game.PlaceId == 11705040445 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/SoapClient/main/DBBV6.lua", true))()
+elseif game.PlaceId == 8750997647 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/SoapClient/main/TLX.lua", true))()
+elseif game.PlaceId == 9492364312 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zShadowSkilled1/SoapClient/main/JumpRace.lua", true))()
 end
